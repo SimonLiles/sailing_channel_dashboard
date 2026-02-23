b3ffe5bb81544be8e88c979e76b3c2d608cc61a6
SimonLiles <simon@quantknot.com>
2026-02-22 21:56:03 -0500

    Create get_channel_profile.sql

---
+++ sql/04_apps/get_channel_profile.sql
@@ -0,0 +1,10 @@
+SELECT 
+  channel_id, 
+  channel_handle,
+  channel_title, 
+  channel_description, 
+  channel_keywords, 
+  profile_pic, 
+  join_date
+FROM `yt-sailing-dashboard.yt_sailing_data.channel_dimensions`
+WHERE channel_id = @id;
