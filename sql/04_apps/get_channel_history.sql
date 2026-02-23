c11b0052a75a219ab27421a327d4f76d85ad656e
SimonLiles <simon@quantknot.com>
2026-02-22 21:55:54 -0500

    Create get_channel_history.sql

---
+++ sql/04_apps/get_channel_history.sql
@@ -0,0 +1,11 @@
+SELECT 
+  date, 
+  view_count, 
+  video_count,
+  subscriber_count, 
+  daily_new_views, 
+  daily_new_subs, 
+  views_moving_avg_7d
+FROM `yt-sailing-dashboard.yt_sailing_data.fct_daily_performance`
+WHERE channel_id = @id
+ORDER BY date ASC;
