c9db1b01dddf5e91965e6ba31556367d6acd4e9b
SimonLiles <simon@quantknot.com>
2026-03-03 11:15:04 -0500

    Update get_leaderboard_30d.sql
    Added views per video ranking and percentile

--- sql/04_apps/get_leaderboard_30d.sql
+++ sql/04_apps/get_leaderboard_30d.sql
@@ -39,7 +39,8 @@ SELECT
       ARRAY_AGG(m.lifetime_views_per_vid ORDER BY m.date)
     ) AS lifetime_views_per_vid,
     
-    SAFE_DIVIDE(SUM(m.daily_new_views), SUM(m.daily_new_videos)) AS views_per_vid_30d,
+    -- Algorithm Performance
+    SAFE_DIVIDE(SUM(m.daily_new_views), MAX(m.video_count)) AS views_per_vid_30d,
     
     -- Ranking and percentile for subscriber count
     DENSE_RANK() OVER (ORDER BY MAX(m.subscriber_count) DESC) AS sub_rank,
@@ -62,12 +63,13 @@ SELECT
     ROUND((1 - PERCENT_RANK() OVER (ORDER BY SUM(m.views_moving_avg_7d) DESC)) * 100) AS view_7d_avg_percentile,
     
     -- Ranking and percentile for 30 day views per video
-    /*DENSE_RANK() OVER (
+    DENSE_RANK() OVER (
       ORDER BY SAFE_DIVIDE(SUM(m.daily_new_views), SUM(m.daily_new_videos)) 
       DESC) AS views_per_vid_30d_rank,
     ROUND((1 - PERCENT_RANK() OVER (
       ORDER BY SAFE_DIVIDE(SUM(m.daily_new_views), SUM(m.daily_new_videos)) 
-      DESC)) * 100) AS views_per_vid_30d_percentile,*/
+      DESC)) * 100) AS views_per_vid_30d_percentile,
+      
 
     -- Ranking and perecentile for daily subs
     DENSE_RANK() OVER (ORDER BY SUM(m.daily_new_subs) DESC) AS daily_sub_rank,
